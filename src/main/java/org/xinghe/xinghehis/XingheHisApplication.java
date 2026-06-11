131e3f882202e38ba66f7e2b07975708a7edc631
Xinghe Wang <24373601@student.uwa.edu.au>
2026-06-11 16:32:09 +0800

    fix: add @MapperScan and configure test profile for H2

--- src/main/java/org/xinghe/xinghehis/XingheHisApplication.java
+++ src/main/java/org/xinghe/xinghehis/XingheHisApplication.java
@@ -1,9 +1,11 @@
 package org.xinghe.xinghehis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.xinghe.xinghehis.mapper")
 public class XingheHisApplication {
 
     public static void main(String[] args) {
